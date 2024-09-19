b7d52aa0a8f86e87470cf163d162beddaf62501c
Priyanshu kumar Mishra <104246169+Priyanshu629@users.noreply.github.com>
2024-09-19 13:35:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ databseConnection()
 //setting middlewares
 app.use(cors(
     {
-        origin: ["https://health-tracking-app.netlify.app/","http://localhost:5173"],
+        origin: ["https://health-tracking-app.netlify.app","http://localhost:5173"],
         methods:["GET","POST","PUT","DELETE"]
     }
 ))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
